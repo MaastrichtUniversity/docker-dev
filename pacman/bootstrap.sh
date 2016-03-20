71e4a67eff7727e094969c4657f499d63df11edf
Paul van Schayck <polleke@gmail.com>
2016-03-20 12:13:53 +0100

    Add bower and composer to image. Get rit-pacman and handsontable from volumes, and prepare them

--- pacman/bootstrap.sh
+++ pacman/bootstrap.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 
-cd /var/www/html
+cd /var/www/html/sites/all/modules/pacman && composer update
 
-drush site-install \
+cd /var/www/html/sites/all/modules/handsontable && bower install --allow-root
+
+cd /var/www/html && drush site-install \
 	--db-url=mysql://root:foobar@db:3306/pacman \
 	--yes \
 	--account-name=rit-admin \
 	--account-pass=foobar \
 	--site-name=pacman
 
-drush en \
+cd /var/www/html && drush en \
 	--yes \
-	xml_forms
+	xml_forms \
+	pacman \
+	handsontable
 
 apache2-foreground
