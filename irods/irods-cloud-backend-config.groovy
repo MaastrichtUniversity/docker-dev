9492110faa0141230085b2ced517f01f2ae0e251
Maarten Coonen <m.coonen@maastrichtuniversity.nl>
2016-03-31 15:24:03 +0200

    Added iRODS-Cloud-Backend to Docker image

---
+++ irods/irods-cloud-backend-config.groovy
@@ -0,0 +1,12 @@
+/*
+ *Backend app configuration
+ *For app specific configs, prefix with beconf. for consistency
+ */
+
+
+// use login preset
+beconf.login.preset.host='localhost'
+beconf.login.preset.port=1247
+beconf.login.preset.zone='ritZone'
+beconf.login.preset.auth.type='Standard'
+beconf.login.preset.enabled=true
