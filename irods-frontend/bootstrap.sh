e8ebb943b1e25963cc4898c8736e6809a8528dd1
Maarten Coonen <m.coonen@maastrichtuniversity.nl>
2016-04-05 12:05:11 +0200

    Removed php5, used original globals.js with sed statement

--- irods-frontend/bootstrap.sh
+++ irods-frontend/bootstrap.sh
@@ -8,7 +8,7 @@ service apache2 restart
 
 # Dynamically fill the backend hostname in globals.js with ENV variable from docker-compose.yml
 # TIP: Since hostname contains forward slashes, use | as separator in sed statement
-sed -i "s|insert_hostname_here|$FQDN_HOST|" /var/www/html/irods-cloud-frontend/app/components/globals.js
+sed -i "s|+location.hostname+|$FQDN_HOST|" /var/www/html/irods-cloud-frontend/app/components/globals.js
 
 # this script must end with a persistent foreground process
 tail -f /var/lib/tomcat8/logs/catalina.out
