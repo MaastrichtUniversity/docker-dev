12b81537a28832aa3676e0fa5abaa842015fe4c7
Maarten Coonen <m.coonen@maastrichtuniversity.nl>
2016-04-05 11:47:11 +0200

    Added cloud-browser-frontend to the irods-frontend container. And added FQDN hostname ENV variable

--- irods-frontend/bootstrap.sh
+++ irods-frontend/bootstrap.sh
@@ -3,6 +3,13 @@
 # Start Tomcat8 service
 /var/lib/tomcat8/bin/startup.sh
 
+# Force start of apache2 server
+service apache2 restart
+
+# Dynamically fill the backend hostname in globals.js with ENV variable from docker-compose.yml
+# TIP: Since hostname contains forward slashes, use | as separator in sed statement
+sed -i "s|insert_hostname_here|$FQDN_HOST|" /var/www/html/irods-cloud-frontend/app/components/globals.js
+
 # this script must end with a persistent foreground process
 tail -f /var/lib/tomcat8/logs/catalina.out
 
