197f2c54936a74c54f5d27f14cc3a9ccab68caf1
Maarten Coonen <m.coonen@maastrichtuniversity.nl>
2016-04-08 16:14:46 +0200

    Cleanup: removed irrelevant sed replacement with FQDN_HOST in irods-frontend

--- irods-frontend/bootstrap.sh
+++ irods-frontend/bootstrap.sh
@@ -6,10 +6,6 @@
 # Force start of apache2 server
 service apache2 restart
 
-# Dynamically fill the backend hostname in globals.js with ENV variable from docker-compose.yml
-# TIP: Since hostname contains forward slashes, use | as separator in sed statement
-sed -i "s|+\"location.hostname+\"|$FQDN_HOST|" /var/www/html/irods-cloud-frontend/app/components/globals.js
-
 # this script must end with a persistent foreground process
 tail -f /var/lib/tomcat8/logs/catalina.out
 
