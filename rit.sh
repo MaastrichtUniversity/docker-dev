30a07f5ca6c5fe32b93b85fa6829e05d416acef2
Paul van Schayck <polleke@gmail.com>
2016-06-27 17:55:11 +0200

    Add simple wrapper script for setting RIT_ENV variable

---
+++ rit.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+if [[ -z $RIT_ENV ]]; then
+    RIT_ENV="local"
+
+    if [[ $HOSTNAME == "fhml-srv018" ]]; then
+        RIT_ENV="acc"
+    fi
+
+    if [[ $HOSTNAME == "fhml-srv020" ]]; then
+        RIT_ENV="dev1"
+    fi
+
+    if [[ $HOSTNAME == "fhml-srv021" ]]; then
+        RIT_ENV="dev2"
+    fi
+fi
+
+export RIT_ENV
+
+docker-compose "$@"
