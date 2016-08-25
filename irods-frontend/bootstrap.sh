4a330e441f48ce92bfc95eef24d98166885b6608
Paul van Schayck <polleke@gmail.com>
2016-08-25 13:23:50 +0200

    Start irods-frontend reverse proxy, not restart

--- irods-frontend/bootstrap.sh
+++ irods-frontend/bootstrap.sh
@@ -4,7 +4,7 @@
 /var/lib/tomcat8/bin/startup.sh
 
 # Force start of apache2 server
-service apache2 restart
+service apache2 start
 
 # this script must end with a persistent foreground process
 tail -f /var/lib/tomcat8/logs/catalina.out
