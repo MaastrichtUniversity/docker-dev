0483afc4a8a5936abb4c0d6a4d2217948385a778
Paul van Schayck <polleke@gmail.com>
2016-10-05 16:56:16 +0200

    Possible fix for apache2 not always starting in frontend

--- irods-frontend/bootstrap.sh
+++ irods-frontend/bootstrap.sh
@@ -4,6 +4,7 @@
 /var/lib/tomcat8/bin/startup.sh
 
 # Force start of apache2 server
+rm -f /var/run/apache2/apache2.pid
 service apache2 start
 
 # this script must end with a persistent foreground process
