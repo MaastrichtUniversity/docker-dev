1f8db1e3b1dbf3874ca335f033c004fedfcf6ce7
Maarten Coonen <m.coonen@maastrichtuniversity.nl>
2017-03-20 17:27:23 +0100

    iRODS 4.2.0 for iRES server (RITDEV-164)

--- ires/bootstrap.sh
+++ ires/bootstrap.sh
@@ -10,10 +10,11 @@ source /etc/secrets
 cd /rules && make install
 
 # Update RIT microservices
-cd /microservices && make install
+# TODO: rewrite Make-procedure OR switch to CMake (just like iRODS-developers)
+#cd /microservices && make install
 
 # Update RIT helpers
-cp /helpers/* /var/lib/irods/iRODS/server/bin/cmd/.
+cp /helpers/* /var/lib/irods/msiExecCmd_bin/.
 
 # Mount ingest zones and rawdata
 mkdir -p /mnt/ingest/zones
@@ -26,11 +27,11 @@ if [[ ! -e /var/run/irods_installed ]]; then
 
     if [ -n "$RODS_PASSWORD" ]; then
         echo "Setting irods password"
-        sed -i "17s/.*/$RODS_PASSWORD/" /etc/irods/setup_responses
+        sed -i "16s/.*/$RODS_PASSWORD/" /opt/irods/setup_responses
     fi
 
     # set up iRODS
-    /opt/irods/config.sh /etc/irods/setup_responses
+    python /var/lib/irods/scripts/setup_irods.py < /opt/irods/setup_responses
 
     # Add the ruleset-rit to server config
     /opt/irods/prepend_ruleset.py /etc/irods/server_config.json rit-misc
@@ -71,4 +72,4 @@ service rmd restart
 /etc/init.d/filebeat start
 
 # this script must end with a persistent foreground process
-tail -F /var/lib/irods/iRODS/server/log/rodsLog.*
+tail -F /var/lib/irods/log/rodsLog.*
