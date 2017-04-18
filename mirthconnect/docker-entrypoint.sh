34fa75e81f2069d0c734bf0b0a9419efcb437437
Maarten Coonen <m.coonen@maastrichtuniversity.nl>
2017-04-18 17:11:35 +0200

    Removed temporary mpi-workaround (RITDEV-283) AND exposed port 6671 for iRODS REST HTTP listener channel (RITDEV-259)

--- mirthconnect/docker-entrypoint.sh
+++ mirthconnect/docker-entrypoint.sh
@@ -18,25 +18,6 @@ psql -h irods-db -U postgres -d postgres <<- EOSQL
     GRANT ALL PRIVILEGES ON DATABASE mirthdb TO mirthconnect;
 EOSQL
 
-until psql -h mpi-tempdb -U postgres -c '\l'; do
-  >&2 echo "Postgres MPI is unavailable - sleeping"
-  sleep 1
-done
-
-psql -h mpi-tempdb -U postgres -d postgres <<- EOSQL
-    CREATE USER mpi WITH PASSWORD 'foobar';
-    CREATE DATABASE mpitempdb;
-    GRANT ALL PRIVILEGES ON DATABASE mpitempdb TO mpi;
-EOSQL
-psql -h mpi-tempdb -U mpi -d mpitempdb <<- EOSQL
-    CREATE TABLE mpilookup(
-        id SERIAL,
-        orgid CHAR(50),
-        euid CHAR(50)
-    );
-EOSQL
-
-
 # Templating of the configuration.properties file
  sed -i "s/RIT_ENV/$RIT_ENV/" /opt/mirth-connect/appdata/configuration.properties
 
