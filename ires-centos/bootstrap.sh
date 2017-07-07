a28d0040c4c8a36a0e0c993242727594ec36f061
Maarten Coonen <m.coonen@maastrichtuniversity.nl>
2017-07-07 09:26:30 +0200

    Changed 'start' into 'restart' for RMD and Filebeat

--- ires-centos/bootstrap.sh
+++ ires-centos/bootstrap.sh
@@ -64,9 +64,9 @@ fi
 
 # Copy the service-control scripts outside of /etc/init.d folder to prevent D-Bus kicking in & force the service to start
 # Metalnx RMD
-cp /etc/init.d/rmd /opt/rmd/rmd && /opt/rmd/rmd start
+cp /etc/init.d/rmd /opt/rmd/rmd && /opt/rmd/rmd restart
 # Logstash
-cp /etc/init.d/filebeat /opt/filebeat && /opt/filebeat start
+cp /etc/init.d/filebeat /opt/filebeat && /opt/filebeat restart
 
 # this script must end with a persistent foreground process
 tail -F /var/lib/irods/iRODS/server/log/rodsLog.*
