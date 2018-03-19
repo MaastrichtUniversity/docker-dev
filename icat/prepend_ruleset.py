ea6f0826cf6fa0fa58fbec75201aba9cd9d9e889
Paul van Schayck <polleke@gmail.com>
2018-03-19 15:36:44 +0100

    Add check to prepend_ruleset to make sure the rule also exists when adding

--- icat/prepend_ruleset.py
+++ icat/prepend_ruleset.py
@@ -4,8 +4,13 @@
 
 import json
 import sys
+import os
 
 def main(path_to_server_config, name_of_new_rule_file):
+    path_rule_file = os.path.join(os.sep, 'etc', 'irods', name_of_new_rule_file + '.re')
+    if not os.path.exists(path_rule_file):
+        sys.exit('Ruleset "%s" does not exist' % path_rule_file)
+
     with open(path_to_server_config, 'r+') as f:
         server_config = json.load(f)
         server_config['re_rulebase_set'].insert(0, {'filename': name_of_new_rule_file})
