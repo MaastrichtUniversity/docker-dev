5bf6da8cdb0904db304128e010df32b2a128e065
rogerniesten <r.niesten@maastrichtuniversity.nl>
2019-01-28 11:11:29 +0100

    enhancements to dh-env

--- rit.sh
+++ rit.sh
@@ -7,6 +7,17 @@ if [[ -z $DH_ENV_HOME ]]; then
 fi
 . $DH_ENV_HOME/lib-dh.sh
 
+# Set logging level based on -v (--verbose) or -vv param
+ARGS="$@ "
+if [[ ${ARGS} = *"-vv "* ]]; then
+   export LOGTRESHOLD=$DBG
+   ARGS="${ARGS/-vv /}"
+elif [[ ${ARGS} = *"--verbose "* ]] || [[ ${ARGS} = *"-v "* ]]; then
+   export LOGTRESHOLD=$INF
+   ARGS="${ARGS/--verbose /}"
+   ARGS="${ARGS/-v /}"
+fi
+
 # Set the prefix for the project
 COMPOSE_PROJECT_NAME="corpus"
 export COMPOSE_PROJECT_NAME
@@ -37,7 +48,7 @@ externals/epicpid-microservice git@github.com:MaastrichtUniversity/epicpid-micro
 
 # do the required action in case of externals or exec
 if [[ $1 == "externals" ]]; then
-    action=${@/$1/}
+    action=${ARGS/$1/}
     run_repo_action ${action} "${externals}"
     exit 0
 fi
@@ -97,7 +108,7 @@ env_selector
 
 
 # Assuming docker-compose is available in the PATH
-log $DBG "$0 [docker-compose \"$@\"]"
-docker-compose "$@"
+log $DBG "$0 [docker-compose \"$ARGS\"]"
+docker-compose "$ARGS"
 
 
