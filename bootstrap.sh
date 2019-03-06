d2c2b76b528973a2474f6ee212414b05ce8cc0e5
Sebastiaan <s.nijhuis@maastrichtuniversity.nl>
2019-03-06 17:24:27 +0100

    Creating missing dirs from bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,6 +2,18 @@
 
 source .env
 
+# Create dirs
+
+if [[ ! -e ${VOLUMES_PATH} ]]; then
+  echo "Creating ${VOLUMES_PATH}"
+  mkdir -p VOLUMES_PATH
+fi
+
+if [[ ! -e ${DISKS_PATH} ]]; then
+  echo "Creating ${DISKS_PATH}"
+  mkdir -p DISKS_PATH
+fi
+
 # remove previous Ceph config
 echo "Removing old Ceph config from ${VOLUMES_PATH}/ceph/"
 sudo rm -rf ${VOLUMES_PATH}/ceph/
