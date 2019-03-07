91e748c5ee64d5a8847e3aad6d6071dc5f9bbab6
Alan King <alanking@renci.org>
2019-03-07 12:31:00 -0500

    Tweaks for bootstrapping

--- bootstrap.sh
+++ bootstrap.sh
@@ -19,10 +19,11 @@ echo "Removing old Ceph config from ${VOLUMES_PATH}/ceph/"
 sudo rm -rf ${VOLUMES_PATH}/ceph/
 
 echo "removing existing loop devices"
+osd_devices=(${OSD1_DEVICE} ${OSD2_DEVICE} ${OSD3_DEVICE})
 # remove existing loop devices
-sudo losetup -d ${OSD1_DEVICE}
-sudo losetup -d ${OSD2_DEVICE}
-sudo losetup -d ${OSD3_DEVICE}
+for i in ${osd_devices[@]}; do
+    sudo losetup -d $i
+done
 
 echo "Clean up old disk files"
 # Clean up old disks
@@ -43,7 +44,7 @@ echo "Creating new loop devices"
 # Create new loop devives
 j=0
 for i in ceph{a..c}; do
-  sudo losetup /dev/loop${j}  ${DISKS_PATH}/$i
+  sudo losetup ${osd_devices[${j}]}  ${DISKS_PATH}/$i
   ((j++));
 done
 
