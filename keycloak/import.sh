89ac2df21a54d86cb21a83cc6efafe3681f0e246
Paul van Schayck <polleke@gmail.com>
2020-04-30 10:26:50 +0200

    Initial work to merge Daniels LDAP work from RITDEV-683. DHS-245

--- keycloak/import.sh
+++ keycloak/import.sh
@@ -35,11 +35,16 @@ echo "Set the correct DEV envonment"
 # Set the correct DEV envonment
 sed 's/RIT_ENV/'"$RIT_ENV"'/g' /tmp/realm-export.json > /tmp/realm-export_env.json
 
+echo "Set the correct ldap admin password envonment"
+# Set the correct ldap admin password envonment
+sed 's/\*\*\*\*\*\*\*\*\*\*/'"$LDAP_ADMIN_PASSWORD"'/g'  /tmp/realm-export_env.json >  /tmp/realm-export_env_pw.json
+
+
 # If drupal realm does not yet exist load it from config
 if ! $(/opt/jboss/keycloak/bin/kcadm.sh get realms | grep -q "drupal");
 then
   echo "Import Drupal realm"
-  /opt/jboss/keycloak/bin/kcadm.sh create realms -f /tmp/realm-export_env.json
+  /opt/jboss/keycloak/bin/kcadm.sh create realms -f /tmp/realm-export_env_pw.json
 fi
 
 #Create Users
@@ -69,3 +74,10 @@ for user in $scannexus; do
 done
 
 echo "Users Created"
+
+# Sync of the ldap
+
+echo "Syncing LDAP"
+/opt/jboss/keycloak/bin/kcadm.sh create user-storage/10d55377-d139-4865-bd3e-1375ea079925/sync?action=triggerFullSync -r drupal
+
+echo "Done syncing LDAP"
