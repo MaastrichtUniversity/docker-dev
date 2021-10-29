e8e5c8b327d4e4f41a1ced8dbcf6e742f626152e
Dean Linssen <d.linssen@maastrichtuniversity.nl>
2021-10-29 10:02:18 +0200

    Added max_project_number avu to bootstrap_irods.sh [DHS-1893, DHS-1940]

--- icat/bootstrap_irods.sh
+++ icat/bootstrap_irods.sh
@@ -186,3 +186,6 @@ nonSyncGroups="rodsadmin DH-ingest public DH-project-admins"
 for group in $nonSyncGroups; do
     imeta add -u "${group}" ldapSync false
 done
+
+# Set the default value vor max_project_number
+imeta add -C /nlmumc/projects max_project_number 11
