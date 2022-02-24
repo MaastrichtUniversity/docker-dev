abfb415e493e63da5c353a4860fbe04ef1d85c2c
Luis Sago <lws.sago@gmail.com>
2022-02-24 13:27:53 +0100

    Add ires-s3 base-derived images
    Note: ires-s3-2 doesn't seem to fully work?
    Feb 24 13:05:01 pid:504 remote addresses: 172.20.0.3, 172.20.0.8 ERROR: [-]	/irods_plugin/s3/libirods_s3.cpp:1908:irods::error s3StartOperation(irods::plugin_property_map &) :  status [SYS_CONFIG_FILE_ERR]  errno [] -- message [[resource_name=UM-Ceph-S3-AC] Unknown error in authorization file.]
    	[-]	/irods_plugin/s3/libirods_s3.cpp:546:irods::error s3ReadAuthInfo(irods::plugin_property_map &) :  status [SYS_CONFIG_FILE_ERR]  errno [] -- message [[resource_name=UM-Ceph-S3-AC] Unknown error in authorization file.]
    
    [DHDO-163]

--- irods/build.sh
+++ irods/build.sh
@@ -8,6 +8,12 @@ cd "$(dirname "$BASH_SOURCE")"
 
 # We first build the irods-base image
 #docker-compose build $@ irods-base-ubuntu irods-icat
-docker-compose build $@ irods-base-ubuntu irods-icat irods-ires
+#docker-compose build $@
+
+for image in irods-base-ubuntu irods-icat irods-ires irods-ires-s3; do
+    echo
+    echo "Building: $image"
+    docker-compose build $@ $image
+done
 
 # Then icat, ires, ires-s3, ires-centos
---
+++ irods/ires-s3/scripts/bootstrap_irods.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/env bash
+
+############
+## Resources
+
+# Create coordination- and child-resources for project data
+iadmin mkresc ${ENV_S3_RESC_NAME} s3 ${HOSTNAME}:/dh-irods-bucket-dev "S3_DEFAULT_HOSTNAME=${ENV_S3_HOST};S3_AUTH_FILE=/var/lib/irods/minio.keypair;S3_REGIONNAME=irods-dev;S3_RETRY_COUNT=1;S3_WAIT_TIME_SEC=3;S3_PROTO=HTTP;ARCHIVE_NAMING_POLICY=consistent;HOST_MODE=cacheless_attached;S3_CACHE_DIR=/cache"
+
+# Sleep for a varying amount (3 * last digit of hostname) of seconds to prevent simultaneous checking for existence of replResc
+SIMPLE_HOSTNAME=${HOSTNAME%%.dh.local}
+sleep `expr ${SIMPLE_HOSTNAME: -1} \* 3`
+
+# Check if repl resource exists, if not, create it
+if [ "$(iadmin lr replRescUMCeph01)" == "No rows found" ];
+then
+  iadmin mkresc replRescUMCeph01 replication;
+  iadmin modresc replRescUMCeph01 comment Replicated-Ceph-resource-for-UM
+  imeta add -R replRescUMCeph01 NCIT:C88193 0.062
+else
+  echo "Replication resource already exists";
+fi
+
+# Add child resource to repl resource
+iadmin addchildtoresc replRescUMCeph01 ${ENV_S3_RESC_NAME}
+
+# Add comment to resource for better identification in MDR's createProject dropdown
+iadmin modresc ${HOSTNAME%%.dh.local}Resource comment DO-NOT-USE
+
+# Add storage pricing to resources
+imeta add -R ${HOSTNAME%%.dh.local}Resource NCIT:C88193 999
+
+###########
+## Projects and project permissions
+
+for i in {01..2}; do
+    PROJECTNAME=$(fortune | head -n 1 | sed 's/\x27/ /g')
+    project=$(irule -F /rules/projects/createProject.r "*authorizationPeriodEndDate='1-1-2018'" "*dataRetentionPeriodEndDate='1-1-2018'" "*ingestResource='iresResource'" "*resource='replRescUMCeph01'" "*storageQuotaGb='10'" "*title='(s3) ${PROJECTNAME}'" "*principalInvestigator='psuppers'" "*dataSteward='pvanschay2'" "*respCostCenter='UM-30001234X'" "*openAccess='false'" "*tapeArchive='true'" "*tapeUnarchive='false'")
+
+    # Manage access
+    ichmod -r own "psuppers" /nlmumc/projects/${project}
+
+    # Data Steward gets manager rights
+    ichmod -r own "pvanschay2" /nlmumc/projects/${project}
+
+    # Contributor access
+    ichmod -r write datahub /nlmumc/projects/${project}
+
+    # Viewer access
+done
+
+##########
+## Special
+
