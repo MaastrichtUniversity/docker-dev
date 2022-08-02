45155324498bb7709c70469412df76f84d7fac56
JonathanMELIUS <meliusjonathan@gmail.com>
2022-08-02 15:42:44 +0200

    Add exit code wrapper for run_put_ingest_job.sh [DHS-2438, DHS-2494]

--- irods/ires/scripts/run_put_ingest_job.sh
+++ irods/ires/scripts/run_put_ingest_job.sh
@@ -9,6 +9,16 @@ echo $1
 echo $2
 echo $3
 
-python -m irods_capability_automated_ingest.irods_sync start $1 $2 --event_handler $3 --synchronous --progress --ignore_cache --log_filename /tmp/tuto_reg.log
+python -m irods_capability_automated_ingest.irods_sync start $1 $2 --event_handler $3 --synchronous --progress --ignore_cache --log_filename /tmp/ingest_put.log
+
+if [ $? -eq 0 ]
+then
+  echo "The script ran ok"
+  exit 0
+else
+  echo $?
+  echo "The script failed" >&2
+  exit 1
+fi
 
 deactivate
