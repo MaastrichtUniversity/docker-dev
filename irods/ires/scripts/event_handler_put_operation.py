7bffa4f61569da3bf85fbd22b2cb041a1ad37216
JonathanMELIUS <meliusjonathan@gmail.com>
2022-08-02 15:42:08 +0200

    Add custom max_retries & delay methods [DHS-2438, DHS-2494]

--- irods/ires/scripts/event_handler_put_operation.py
+++ irods/ires/scripts/event_handler_put_operation.py
@@ -5,8 +5,12 @@ from irods_capability_automated_ingest.utils import Operation
 class event_handler(Core):
 
     @staticmethod
-    def to_resource(session, meta, **options):
-        return "replRescUM01"
+    def max_retries(hdlr_mod, logger, meta):
+        return 3
+
+    @staticmethod
+    def delay(hdlr_mod, logger, meta, retries):
+        return 15
 
     @staticmethod
     def operation(session, meta, **options):
