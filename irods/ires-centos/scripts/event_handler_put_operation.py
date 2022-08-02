40c26f2c0e37c3dff7e05f08b7e2a2ceed7afc69
JonathanMELIUS <meliusjonathan@gmail.com>
2022-08-02 15:51:38 +0200

    Add retry in ires-centos [DHS-2438, DHS-2494]

--- irods/ires-centos/scripts/event_handler_put_operation.py
+++ irods/ires-centos/scripts/event_handler_put_operation.py
@@ -5,5 +5,13 @@ from irods_capability_automated_ingest.utils import Operation
 class event_handler(Core):
 
     @staticmethod
+    def max_retries(hdlr_mod, logger, meta):
+        return 3
+
+    @staticmethod
+    def delay(hdlr_mod, logger, meta, retries):
+        return 15
+
+    @staticmethod
     def operation(session, meta, **options):
         return Operation.PUT
