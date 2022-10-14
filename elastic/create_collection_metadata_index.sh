45600cc925b2f70a1b30d7c21aa719db92a470d0
Daniel Theunissen <d.theunissen@maastrichtuniversity.nl>
2022-10-14 10:30:09 +0200

    Add a named volume to elastic for data persistence [DHS-2524, DHS-2527]

--- elastic/create_collection_metadata_index.sh
+++ elastic/create_collection_metadata_index.sh
@@ -8,7 +8,7 @@ for EXPONENTIAL_BACKOFF in {1..10}; do
 done
 
 
-curl -u elastic:$ELASTIC_PASSWORD -X GET "localhost:9200/_cluster/health?wait_for_status=green&timeout=300s&pretty" | grep green
+curl -u elastic:$ELASTIC_PASSWORD -X GET "localhost:9200/_cluster/health?wait_for_status=yellow&timeout=300s&pretty" | grep yellow
 # Wait 300 seconds for elastic to have status green
 if ! [ $? -eq 0  ];then
     echo "Unable to connect to elastic"
