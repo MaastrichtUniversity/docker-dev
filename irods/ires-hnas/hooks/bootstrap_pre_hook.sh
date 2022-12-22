8c125479afe16f4971a9d9624003140d96efca77
Luis Sago <lws.sago@gmail.com>
2022-12-22 14:26:47 +0100

    Fix: apply dev setup_irods patch for ires-hnas
    [DHDO-724]

--- irods/ires-hnas/hooks/bootstrap_pre_hook.sh
+++ irods/ires-hnas/hooks/bootstrap_pre_hook.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-# NOTE: At the moment, every restart, this will be called! So make sure that these hooks are idempotent.
-#       We could leverage irods_startup_state_get() here, but for now we leave like this..
+source /opt/irods/lib/irods.sh
+
+echo "INFO: Applying setup_irods_already_installed_dev.patch to setup_irods.py"
+echo "INFO: This patch will mkresc if the resource is not already registered and comment out test_put()"
+patch_setup_irods /opt/irods/patch/setup_irods_already_installed_dev.patch
 
 # Create fake HNAS (nothing real is volume mapped in development)
 if [[ ! -d "/mnt/${ENV_IRODS_STOR_RESC_NAME}" ]]; then
