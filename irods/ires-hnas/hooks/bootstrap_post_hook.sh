47da9669c76e6ce0db22ba5ecfa5856cd7c754b9
jonathanMELIUS <meliusjonathan@gmail.com>
2023-05-17 16:40:44 +0200

    WIP - Create S3 resources in iCAT [DHS-2931, DHS-3063]

--- irods/ires-hnas/hooks/bootstrap_post_hook.sh
+++ irods/ires-hnas/hooks/bootstrap_post_hook.sh
@@ -4,3 +4,6 @@
 
 echo "INFO: Executing bootstrap_irods.sh"
 su irods -c "/opt/irods/hooks/bootstrap_irods.sh"
+#mkdir -p /var/lib/irods/log/
+#echo "hello" > /var/lib/irods/log/reLog.test.txt
+#chmod -R 777 /var/lib/irods/log/
