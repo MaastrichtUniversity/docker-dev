8dfc011d9b8c8b4cbb75728043aec805841aa5f3
Dean Linssen <d.linssen@maastrichtuniversity.nl>
2023-05-25 13:26:57 +0200

    Move S3 resource creation to ires-hnas-um [DHS-2931,DHS-3063]

--- irods/icat/hooks/bootstrap_post_hook.sh
+++ irods/icat/hooks/bootstrap_post_hook.sh
@@ -12,9 +12,3 @@ chmod 755 /var/lib/irods/msiExecCmd_bin/dm*
 
 echo "INFO: Executing bootstrap_irods.sh"
 su irods -c "/opt/irods/hooks/bootstrap_irods.sh"
-
-echo "bootstrap_s3_ires $ENV_S3_RESC_NAME_AC  $ENV_S3_HOST_AC  $ENV_S3_AUTH_FILE_AC"
-su irods -c "/opt/irods/hooks/bootstrap_s3_ires.sh $ENV_S3_RESC_NAME_AC  $ENV_S3_HOST_AC  $ENV_S3_AUTH_FILE_AC"
-
-echo "bootstrap_s3_ires $ENV_S3_RESC_NAME_GL  $ENV_S3_HOST_GL  $ENV_S3_AUTH_FILE_GL"
-su irods -c "/opt/irods/hooks/bootstrap_s3_ires.sh $ENV_S3_RESC_NAME_GL  $ENV_S3_HOST_GL  $ENV_S3_AUTH_FILE_GL"
--- irods/ires-hnas/hooks/bootstrap_post_hook.sh
+++ irods/ires-hnas/hooks/bootstrap_post_hook.sh
@@ -7,3 +7,11 @@ su irods -c "/opt/irods/hooks/bootstrap_irods.sh"
 #mkdir -p /var/lib/irods/log/
 #echo "hello" > /var/lib/irods/log/reLog.test.txt
 #chmod -R 777 /var/lib/irods/log/
+
+if [[ "$ENV_IRODS_COOR_RESC_NAME" =~ "UM" ]]; then
+	echo "bootstrap_s3_ires $ENV_S3_RESC_NAME_AC  $ENV_S3_HOST_AC  $ENV_S3_AUTH_FILE_AC"
+	su irods -c "/opt/irods/hooks/bootstrap_s3_ires.sh $ENV_S3_RESC_NAME_AC  $ENV_S3_HOST_AC  $ENV_S3_AUTH_FILE_AC"
+
+	echo "bootstrap_s3_ires $ENV_S3_RESC_NAME_GL  $ENV_S3_HOST_GL  $ENV_S3_AUTH_FILE_GL"
+	su irods -c "/opt/irods/hooks/bootstrap_s3_ires.sh $ENV_S3_RESC_NAME_GL  $ENV_S3_HOST_GL  $ENV_S3_AUTH_FILE_GL"
+fi
