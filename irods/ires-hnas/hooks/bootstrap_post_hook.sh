5e37d95c14c08fd4216a7a63defbc458f2c44b2c
Dean Linssen <d.linssen@maastrichtuniversity.nl>
2023-05-25 14:32:02 +0200

    Refactor bootstrap S3 [DHS-2931, DHS-3063]

--- irods/ires-hnas/hooks/bootstrap_post_hook.sh
+++ irods/ires-hnas/hooks/bootstrap_post_hook.sh
@@ -4,14 +4,9 @@
 
 echo "INFO: Executing bootstrap_irods.sh"
 su irods -c "/opt/irods/hooks/bootstrap_irods.sh"
-#mkdir -p /var/lib/irods/log/
-#echo "hello" > /var/lib/irods/log/reLog.test.txt
-#chmod -R 777 /var/lib/irods/log/
 
+# Also create the S3 resources if we are in the UM container
 if [[ "$ENV_IRODS_COOR_RESC_NAME" =~ "UM" ]]; then
-	echo "bootstrap_s3_ires $ENV_S3_RESC_NAME_AC  $ENV_S3_HOST_AC  $ENV_S3_AUTH_FILE_AC"
-	su irods -c "/opt/irods/hooks/bootstrap_s3_ires.sh $ENV_S3_RESC_NAME_AC  $ENV_S3_HOST_AC  $ENV_S3_AUTH_FILE_AC"
-
-	echo "bootstrap_s3_ires $ENV_S3_RESC_NAME_GL  $ENV_S3_HOST_GL  $ENV_S3_AUTH_FILE_GL"
-	su irods -c "/opt/irods/hooks/bootstrap_s3_ires.sh $ENV_S3_RESC_NAME_GL  $ENV_S3_HOST_GL  $ENV_S3_AUTH_FILE_GL"
+        echo "INFO: Now executing S3 bootstrap"
+	su irods -c "/opt/irods/hooks/bootstrap_s3_ires.sh $ENV_S3_RESC_NAME_AC $ENV_S3_HOST_AC $ENV_S3_AUTH_FILE_AC $ENV_S3_RESC_NAME_GL $ENV_S3_HOST_GL $ENV_S3_AUTH_FILE_GL"
 fi
