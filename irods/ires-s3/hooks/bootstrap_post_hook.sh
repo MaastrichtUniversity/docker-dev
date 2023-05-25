83433e19e2bb84451cf18ba82b93e0e6c7e8922f
Dean Linssen <d.linssen@maastrichtuniversity.nl>
2023-05-25 13:24:39 +0200

    Remove S3 directory [DHS-2931, DHS-3063]

--- irods/ires-s3/hooks/bootstrap_post_hook.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#
-# We create mock dev environment: mock projects and collections to test stuff against
-
-echo "INFO: Executing bootstrap_irods.sh"
-su irods -c "/opt/irods/hooks/bootstrap_irods.sh"
